45679d8dfaeeaee8c27d5c9e8d70fac1c7db317b
cutebot00 <35274958+cutebot00@users.noreply.github.com>
2018-01-10 13:01:34 +0700

    Update chatColor.js

--- chatColor.js
+++ chatColor.js
@@ -1,7 +1,8 @@
-function chatColor() {
+//change font chat color
+setInterval(function(){
 	$('.listTextContainer').each(function() {
 		if ($(this).html().indexOf('style')<=0) {
 			$('span',this).css('color','#ddd')
 		}
 	})
-}
+}, 500)
