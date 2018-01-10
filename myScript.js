e21e93864b0f26491a80da88320a8768345a8243
cutebot00 <35274958+cutebot00@users.noreply.github.com>
2018-01-10 16:56:45 +0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,9 +1,1 @@
-//change font chat color
-var fontColor = "#ccc"
-$('.listTextContainer .message').each(function() {
-	if ( $(this).parent().html().indexOf('color') < 0 ) { //flower
-		$(this).css('color', fontColor)
-	} else if ( $(this).parent().html().indexOf('#000000') > 0 ) { //black font
-		$("span",this).css('color', fontColor)
-	}
-})
+alert()
