11f2bab5f4f57ba1dba7ad84a222e471f3646745
cutebot00 <35274958+cutebot00@users.noreply.github.com>
2018-01-10 13:02:23 +0700

    Delete chatColor.js

--- chatColor.js
+++
@@ -1,8 +0,0 @@
-//change font chat color
-setInterval(function(){
-	$('.listTextContainer').each(function() {
-		if ($(this).html().indexOf('style')<=0) {
-			$('span',this).css('color','#ddd')
-		}
-	})
-}, 500)
